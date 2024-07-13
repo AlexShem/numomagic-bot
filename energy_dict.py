b6fafb6980d9ac2ecb1fbd3877176628a5e4bfb1
dimerz <dimerz@ya.ru>
2024-07-13 20:12:04 +0300

    - Add description for 5 and 6 digits

--- energy_dict.py
+++ energy_dict.py
@@ -2,7 +2,7 @@ import json
 
 
 def load(periods):
-    file = f"{periods}-digits.json"
+    file = f"dicts/{periods}-digits.json"
     with open(file, "r") as f:
         description = json.load(f)
     return description
