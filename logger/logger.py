22f1459d8cbce243dd7e8d6ae1f78516c4c956de
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2024-10-08 18:22:27 +0200

    Add `TimedRotatingFileHandlerWithHeader` for log file header management
    - Introduced `TimedRotatingFileHandlerWithHeader` class to handle log file rotation with a custom header.
    - Modified the `get_logger` function to use the new handler, ensuring headers are written when a new log file is created.
    - Refactored log file initialization to handle the header writing logic within the handler.
    - Cleaned up the conditional logic for creating log directories and files.

--- logger/logger.py
+++ logger/logger.py
@@ -8,6 +8,19 @@ from logging.handlers import TimedRotatingFileHandler
 from dotenv import load_dotenv
 
 
+class TimedRotatingFileHandlerWithHeader(TimedRotatingFileHandler):
+    def __init__(self, filename, header, when='h', interval=1, backupCount=0, encoding=None, delay=False, utc=False, atTime=None):
+        self.header = header
+        super().__init__(filename, when, interval, backupCount, encoding, delay, utc, atTime)
+
+    def doRollover(self):
+        super().doRollover()
+        if not self.delay:
+            self.stream = self._open()
+            self.stream.write(f"{self.header}\n")
+            self.stream.flush()
+
+
 def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.WARNING)
@@ -24,20 +37,21 @@ def get_csv_logger():
     logger.setLevel(logging.INFO)
     load_dotenv()
     log_file = os.path.normpath(os.getenv("LOG_PATH"))
+    fieldnames = ",".join([
+        'timestamp', 'user_id', 'username', 'first_name', 'last_name',
+        'is_premium', 'app_language', 'action', 'action_details'
+    ])
     if not os.path.exists(os.path.dirname(log_file)):
         os.makedirs(os.path.dirname(log_file))
-    if not os.path.exists(log_file):
-        fieldnames = ",".join([
-            'timestamp', 'user_id', 'username', 'first_name', 'last_name',
-            'is_premium', 'app_language', 'action', 'action_details'
-        ])
-        with open(log_file, 'w') as file:
-            file.write(f"{fieldnames}\n")
 
-    handler = TimedRotatingFileHandler(log_file, when="W0", interval=1, backupCount=100)
+    handler = TimedRotatingFileHandlerWithHeader(
+        filename=log_file,
+        header=fieldnames,
+        when="W0",
+        interval=1,
+        backupCount=100
+    )
     formatter = logging.Formatter('%(asctime)s,%(message)s', datefmt='%Y-%m-%d')
-
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-
     return logger
